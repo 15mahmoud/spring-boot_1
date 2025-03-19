3f5c1462d0a3ab689ec43f77cabf3bb9e5fbe4e2
mahmoud <1522003ma@gmail.com>
2025-03-19 17:51:19 +0200

    add&update&delete Employee

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,10 +4,7 @@ import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +29,30 @@ public class EmployeeRestController {
             throw new RuntimeException("Employee id not found - "+employeeId);
         }
         return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        theEmployee.setId(0);
+        Employee dbEmployee=employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee=employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee dbEmployee=employeeService.findById(employeeId);
+        if(dbEmployee == null){
+            throw new RuntimeException("Employee id not found - "+employeeId);
+        }
+        employeeService.delete(employeeId);
+        return "Deleted employee-id "+employeeId;
     }
 
 }
